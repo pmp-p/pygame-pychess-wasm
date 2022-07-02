2254d82230c6852e6bd06346a5852bcbb9acaa4c
pmp-p <mail.peny@free.fr>
2022-07-02 10:54:14 +0200

    wasm port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import pygame
 import sys
 from src.const import *
@@ -14,8 +15,8 @@ class Main:
         pygame.display.set_caption('Chess')
         self.game = Game()
 
-    def mainloop(self):
-        
+    async def mainloop(self):
+
         screen = self.screen
         game = self.game
         board = self.game.board
@@ -49,12 +50,12 @@ class Main:
                             board.calc_moves(piece, clicked_row, clicked_col, bool=True)
                             dragger.save_initial(event.pos)
                             dragger.drag_piece(piece)
-                            # show methods 
+                            # show methods
                             game.show_bg(screen)
                             game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_pieces(screen)
-                
+
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
                     motion_row = event.pos[1] // SQSIZE
@@ -71,10 +72,10 @@ class Main:
                         game.show_pieces(screen)
                         game.show_hover(screen)
                         dragger.update_blit(screen)
-                
+
                 # click release
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
 
@@ -92,7 +93,7 @@ class Main:
                             captured = board.squares[released_row][released_col].has_piece()
                             board.move(dragger.piece, move)
 
-                            board.set_true_en_passant(dragger.piece)                            
+                            board.set_true_en_passant(dragger.piece)
 
                             # sounds
                             game.play_sound(captured)
@@ -102,12 +103,12 @@ class Main:
                             game.show_pieces(screen)
                             # next turn
                             game.next_turn()
-                    
+
                     dragger.undrag_piece()
-                
+
                 # key press
                 elif event.type == pygame.KEYDOWN:
-                    
+
                     # changing themes
                     if event.key == pygame.K_t:
                         game.change_theme()
@@ -117,7 +118,7 @@ class Main:
                         game = self.game
                         board = self.game.board
                         dragger = self.game.dragger
-                    
+
                     if event.key == pygame.K_q:
                         pygame.quit()
                         sys.exit()
@@ -125,9 +126,11 @@ class Main:
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+
             pygame.display.update()
+            await asyncio.sleep(0)
 
+if __name__ == "__main__":
+    main = Main()
+    asyncio.run(main.mainloop())
 
-main = Main()
-main.mainloop()
